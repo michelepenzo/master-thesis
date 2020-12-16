e4efa0d44d4ecbf71f302274fb13ecaaadcabda1
michelepenzo <michelepenzo@outlook.it>
2020-12-16 15:05:15 +0100

    vibrazione su forze esterne

--- pad_control/src/controller.py
+++ pad_control/src/controller.py
@@ -3,33 +3,26 @@
 import rospy
 from sensor_msgs.msg import JoyFeedbackArray, JoyFeedback
 from iiwa_msgs import msg
+from geometry_msgs.msg import Wrench
 
 ON, OFF = 1.0, 0.0
 
 # set feedback joypad
 def set_feedback(value):
-	rum = JoyFeedback()
-	msg = JoyFeedbackArray()
-
-	rum.type, rum.id, rum.intensity = JoyFeedback.TYPE_RUMBLE, 1, value
-	msg.array = [rum]
+	msg, vib = JoyFeedbackArray(), JoyFeedback()
+	vib.type, vib.id, vib.intensity = JoyFeedback.TYPE_RUMBLE, 1, value
+	msg.array = [vib]
 
 	pub.publish(msg)
-	rospy.logwarn(msg)
 
 
-# read external force on robot joints
-def read_external_force(data):
-	#rospy.logwarn(data.torque)
-	pass
+# read external force on end effector
+def read_cartesian_wrench(data):
 
-
-'''
-scrive nel file csv la posa quando premo pulsante
-stessa cosa viene fatta per apertura e chiusura gripper
-tasto play e chiama nodo di play
-'''
-
+	if abs(data.wrench.force.z) < 10 or abs(data.wrench.force.x) > 5 or abs(data.wrench.force.y) > 10:
+		set_feedback(ON)
+	else:
+		set_feedback(OFF)
 
 # ---------------------------------------------------------------------------------------------
 
@@ -38,15 +31,16 @@ if __name__ == '__main__':
 	# init instructions
 	rospy.init_node('controller', disable_signals=True)
 	pub = rospy.Publisher('/joy/set_feedback', JoyFeedbackArray, queue_size=1)
-	sub = rospy.Subscriber('/iiwa/state/ExternalJointTorque', msg.JointTorque, read_external_force)
+	rospy.Subscriber('/iiwa/state/CartesianWrench', msg.CartesianWrench, read_cartesian_wrench)
 
 	try:
-
-		set_feedback(ON)
-
 		while True:
 			rospy.sleep(1)
-
 	except KeyboardInterrupt:
-		rospy.signal_shutdown('')
-		rospy.logwarn('KeyboardInterrupt controller...')
+		pass
+
+	'''
+	scrive nel file csv la posa quando premo pulsante
+	stessa cosa viene fatta per apertura e chiusura gripper
+	tasto play e chiama nodo di play
+	'''
