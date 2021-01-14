d77682e5019386d85b8bdb3c38ac8abe52dd9aad
michelepenzo <michelepenzo@outlook.it>
2021-01-14 13:31:31 +0100

    test task_1 michele_penzo

--- scripts/calculate_data.py
+++ scripts/calculate_data.py
@@ -4,14 +4,13 @@ import pandas as pd
 import rospy, os, csv
 from utils_functions.cartesian_wrench import sample_rate
 
-single_mode = False
-
 # file to read
 path = '/home/ice-admin/iiwa_stack_ws/src/iiwa_stack/pkgs_mp/csv_files/'
 filename_wrench_csv = path + rospy.get_param('mode') + '/' + rospy.get_param('task') + '/' + rospy.get_param('name') + '_wrench.csv'
 
-path_kt_wrench = path + 'kt/' # + rospy.get_param('name') + '_wrench.csv'
-path_teleop_wrench = path + 'teleop/'# + rospy.get_param('name') + '_wrench.csv'
+path_kt_wrench = path + 'kt/'
+path_teleop_wrench = path + 'teleop/'
+
 
 def calculate_intro(s):
 	n_values = s['wrench_x'].count()
@@ -35,23 +34,25 @@ def calculate_values(s):
 # for every *_wrench.csv in same task and mode file calculate min, max, mean and create a new file with values
 def calculate_values_task_mode(file_path, task_n):
 	clean_file(file_path + 'results/' + task_n + '.csv')
+	print_on_csv(('name,mix_x,min_y,min_z,max_x,max_y,max_z,mean_x,mean_y,mean_z',), file_path + 'results/' + task_n + '.csv')
 
 	for filename in os.listdir(file_path + task_n+ '/'):
 
 		if '_wrench' in filename:
 			ws = pd.read_csv(file_path + task_n + '/' + filename, squeeze=True)
 			_min, _max, _mean = calculate_values(ws)
-			print_on_csv((_min, _max, _mean), file_path + 'results/' + task_n + '.csv')
+			print_on_csv((filename, _min, _max, _mean), file_path + 'results/' + task_n + '.csv')
 
 	if 'kt' in file_path:
 		return ' = KT ' + task_n + '.csv OK'
 	else:
 		return ' = Teleop ' + task_n + '.csv OK'
 
+
 # print on csv file in results folder
 def print_on_csv(data, filename):
 	with open(filename, 'ab') as outfile:
-		wr = csv.writer(outfile, quoting=csv.QUOTE_NONE, delimiter=',', quotechar='',escapechar='\\')
+		wr = csv.writer(outfile, quoting=csv.QUOTE_ALL)#, delimiter=',', quotechar='', escapechar='\\')
 		wr.writerow(data)
 
 
@@ -65,10 +66,10 @@ def clean_file(filename):
 
 if __name__ == '__main__':
 
-	# TODO distinguere in base alla modalita
+	single_mode = True
+
 	if single_mode:
-		print('\n = Calculating data of {' + rospy.get_param('name') + '} performing {' + rospy.get_param(
-		'task') + '} with {' + rospy.get_param('mode') + '} \n')
+		print('\n = Calculating data of {' + rospy.get_param('name') + '} performing {' + rospy.get_param('task') + '} with {' + rospy.get_param('mode') + '} \n')
 
 		# reading single csv_file
 		single_file = pd.read_csv(filename_wrench_csv, squeeze=True)
