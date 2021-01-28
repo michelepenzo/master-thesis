0cbfc8d154da2bac8b13f7ab37180c49c167e88d
michelepenzo <michelepenzo@outlook.it>
2021-01-28 15:35:35 +0100

    exeperimental design

--- utils_functions/src/utils_functions/collection_data.py
+++ utils_functions/src/utils_functions/collection_data.py
@@ -10,21 +10,18 @@ sample_rate = 0.1
 
 actual_wrench = msg.CartesianWrench().wrench.force
 actual_cartesian_pose = msg.CartesianPose().poseStamped.pose
-'''
 actual_external_joint_torque = msg.JointTorque().torque
 actual_joint_position = msg.JointPosition().position
 actual_joint_velocity = msg.JointVelocity().velocity
 actual_joint_torque = msg.JointTorque().torque
-'''
-
 
 filename_wrench_csv = path + '_wrench.csv'
 filename_pose_csv = path + '_pose.csv'
-'''
 filename_ext_torque_csv = path + '_ext_torque.csv'
 filename_torque_csv = path + '_torque.csv'
 filename_position_csv = path + '_joint_pos.csv'
 filename_vel_csv = path + '_velocity.csv'
+
 
 def read_external_joint_torque(data):
 	global actual_external_joint_torque
@@ -44,7 +41,6 @@ def read_joint_velocity(data):
 def read_joint_torque(data):
 	global actual_joint_torque
 	actual_joint_torque = data.torque
-'''
 
 
 def read_cartesian_wrench(data):
@@ -65,32 +61,26 @@ if __name__ == '__main__':
 	rospy.init_node('collection_data', disable_signals=True)
 	rospy.Subscriber('/iiwa/state/CartesianWrench', msg.CartesianWrench, read_cartesian_wrench)
 	rospy.Subscriber('/iiwa/state/CartesianPose', msg.CartesianPose, read_cartesian_pose)
-	'''
 	rospy.Subscriber('/iiwa/state/ExternalJointTorque', msg.JointTorque, read_external_joint_torque)
 	rospy.Subscriber('/iiwa/state/JointTorque', msg.JointTorque, read_joint_torque)
 	rospy.Subscriber('/iiwa/state/JointPosition', msg.JointPosition, read_joint_position)
 	rospy.Subscriber('/iiwa/state/JointVelocity', msg.JointVelocity, read_joint_velocity)
-	'''
 
 	clean_file_datas(filename_wrench_csv)
 	clean_file_datas(filename_pose_csv)
-	'''
 	clean_file_datas(filename_ext_torque_csv)
 	clean_file_datas(filename_torque_csv)
 	clean_file_datas(filename_position_csv)
 	clean_file_datas(filename_vel_csv)
-	'''
 
 	try:
 		print_on_csv_datas(('wrench_x', 'wrench_y', 'wrench_z'), filename_wrench_csv)
 		print_on_csv_datas(('position_x', 'position_y', 'position_z', 'orientation_x',
 							'orientation_y', 'orientation_z', 'orientation_w'), filename_pose_csv)
-		'''
 		print_on_csv_datas(('a1', 'a2', 'a3', 'a4', 'a5', 'a5', 'a7'), filename_ext_torque_csv)
 		print_on_csv_datas(('a1', 'a2', 'a3', 'a4', 'a5', 'a5', 'a7'), filename_torque_csv)
 		print_on_csv_datas(('a1', 'a2', 'a3', 'a4', 'a5', 'a5', 'a7'), filename_position_csv)
 		print_on_csv_datas(('a1', 'a2', 'a3', 'a4', 'a5', 'a5', 'a7'), filename_vel_csv)
-		'''
 
 		while True:
 			rospy.sleep(sample_rate)
@@ -102,7 +92,7 @@ if __name__ == '__main__':
 			print_on_csv_datas((actual_cartesian_pose.position.x, actual_cartesian_pose.position.y, actual_cartesian_pose.position.z,
 				 				actual_cartesian_pose.orientation.x, actual_cartesian_pose.orientation.y,
 				 				actual_cartesian_pose.orientation.z, actual_cartesian_pose.orientation.w), filename_pose_csv)
-			'''
+
 			# external torque
 			print_on_csv_datas((actual_external_joint_torque.a1, actual_external_joint_torque.a2, actual_external_joint_torque.a3,
 								actual_external_joint_torque.a4, actual_external_joint_torque.a5, actual_external_joint_torque.a6,
@@ -122,7 +112,7 @@ if __name__ == '__main__':
 			print_on_csv_datas((actual_joint_torque.a1, actual_joint_torque.a2, actual_joint_torque.a3,
 								actual_joint_torque.a4, actual_joint_torque.a5, actual_joint_torque.a6,
 								actual_joint_torque.a7), filename_torque_csv)
-			'''
+
 
 	except KeyboardInterrupt:
 
