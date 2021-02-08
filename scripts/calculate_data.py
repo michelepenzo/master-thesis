6bebfa00cae717b8e01ae389e13f10ed88c26b38
michelepenzo <michelepenzo@outlook.it>
2021-02-08 11:01:40 +0100

    waypoints files

--- scripts/calculate_data.py
+++ scripts/calculate_data.py
@@ -9,7 +9,7 @@ sample_rate = 0.1
 
 # LOCAL
 path = '/home/michele/Documents/robotica/csv_files/'
-users = ['user_1', 'user_2', 'user_3', 'user_4', 'user_5', 'user_6']  # , 'user_7', 'user_8', 'user_9', 'user_10']
+users = ['user_1', 'user_2', 'user_3', 'user_4', 'user_5', 'user_6', 'user_7', 'user_8', 'user_9', 'user_10']
 reps = ['rep_1', 'rep_2', 'rep_3']
 tasks = ['task_2', 'task_3']
 modes = ['kt', 'teleop']
@@ -60,6 +60,27 @@ def calculate_time(mode, task):
 		out.clear()
 
 
+# for every user_x.csv file save only waypoints for simplify the count
+def save_waypoints():
+	n_file = 0
+
+	for m in range(len(modes)):
+		for t in range(len(tasks)):
+			for r in range(len(reps)):
+				for u in range(len(users)):
+
+					filename = path + modes[m] + '/' + tasks[t] + '/' + reps[r] + '/' + users[u] + '.csv'
+					filename_wp = path + modes[m] + '/' + tasks[t] + '/' + reps[r] + '/' + users[u] + '_waypoints.csv'
+					with open(filename, 'r') as f:
+						with open(filename_wp, 'w') as fwp:
+							for line in f.readlines():
+								if 'pose' in line:
+									fwp.write(line)
+
+					n_file += 1
+
+	print(n_file)
+
 # ---------------------------------------------------------------------------------------------
 
 
@@ -70,30 +91,32 @@ if __name__ == '__main__':
 	single_file = pd.read_csv(filename_wrench_csv, squeeze=True)
 	print('\n = Calculating data of {' + name + '} performing {' + task + '} at {' + rep + '} with {' + mode + '} \n')
 	calculate_time_single_user(single_file)
-	'''
+	
 
 	for m in range(len(modes)):
 		for t in range(len(tasks)):
 			calculate_time(mode=modes[m], task=tasks[t])
 			print('= Calculating data of {' + tasks[t] + '} with {' + modes[m] + '}')
+	'''
 
 	# ==== PLOT DI TEMPI SU TRE RIPETIZIONI ====
-
+	
 	# task2, teleop
 	times_csv = path + 'results/times/' + modes[1] + '_' + tasks[0] + '_' + 'times.csv'
 	s = pd.read_csv(times_csv, squeeze=True)
 	plt.subplot(221)
-	plt.plot(s['user_1']); plt.plot(s['user_2']); plt.plot(s['user_3']);plt.plot(s['user_4']); plt.plot(s['user_5'])
-	plt.plot(s['user_6'])#; plt.plot(s['user_7']); plt.plot(s['user_8']);plt.plot(s['user_9']); plt.plot(s['user_10'])
+
+	plt.plot(s['user_1']); plt.plot(s['user_2']); plt.plot(s['user_3']); plt.plot(s['user_4']); plt.plot(s['user_5'])
+	plt.plot(s['user_6'])#; #plt.plot(s['user_7']); plt.plot(s['user_8']); plt.plot(s['user_9']); plt.plot(s['user_10'])
 
 	# task3, teleop
 	times_csv = path + 'results/times/' + modes[1] + '_' + tasks[1] + '_' + 'times.csv'
 	s = pd.read_csv(times_csv, squeeze=True)
 	plt.subplot(222)
 	plt.plot(s['user_1']); plt.plot(s['user_2']); plt.plot(s['user_3']); plt.plot(s['user_4']); plt.plot(s['user_5'])
-	plt.plot(s['user_6'])#; plt.plot(s['user_7']); plt.plot(s['user_8']);plt.plot(s['user_9']); plt.plot(s['user_10'])
-
-
-
+	plt.plot(s['user_6']); plt.plot(s['user_7']); plt.plot(s['user_8']);plt.plot(s['user_9']); plt.plot(s['user_10'])
 
 	plt.show()
+
+
+	#save_waypoints()
